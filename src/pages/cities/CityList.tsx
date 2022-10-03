2c13ef3c80930ce6a31826f27ebc1835ffacebbd
samuel <samuelaoliveiraramos@gmail.com>
2022-10-03 09:25:25 -0300

    feat: integração da busca com a url via react-router-dom

--- src/pages/cities/CityList.tsx
+++ src/pages/cities/CityList.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
 import { Toolbar } from "../../shared/components";
 import { LayoutBasePage } from "../../shared/layouts";
 
 export const CityList: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const search = useMemo(
+    () => searchParams.get("search") || "",
+    [searchParams],
+  );
+
   return (
     <LayoutBasePage
       title="Listagem de cidades"
-      toolbar={<Toolbar textButtonNew="Nova" showSearchInput />}
-      // #24 // time: 16:40
+      toolbar={
+        <Toolbar
+          textButtonNew="Nova"
+          showSearchInput
+          searchText={search}
+          toggleTextSearch={(text) =>
+            setSearchParams({ search: text }, { replace: true })
+          }
+        />
+      }
     >
       {""}
     </LayoutBasePage>
