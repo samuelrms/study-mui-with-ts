2d4cf91fde0ad46daeed7ed9f24118918f554f9e
samuelrms <samuelaoliveiraramos@gmail.com>
2022-10-13 21:26:23 -0300

    feat: inicio da criação de login

---
+++ src/shared/services/api/auth/AuthService.ts
@@ -0,0 +1,27 @@
+import { API } from "../axiosConfig";
+
+interface authProps {
+  accessToken: string;
+}
+
+const auth =
+  () =>
+  async (email: string, password: string): Promise<authProps | Error> => {
+    try {
+      const { data } = await API.get("auth", { data: { email, password } });
+
+      if (data) {
+        return data;
+      }
+      return new Error("Erro no login");
+    } catch (error) {
+      console.error(error);
+      return new Error(
+        (error as { massage: string }).massage || "Erro no login",
+      );
+    }
+  };
+
+export const AuthService = {
+  auth,
+};
